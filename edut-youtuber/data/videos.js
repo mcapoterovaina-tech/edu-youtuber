3ca6e6cfcc1c7f064ab165cc3837a4bed6737e93
mcapoterovaina-tech <mcapoterovaina@gmail.com>
2025-08-19 13:38:08 -0400

    Update videos.js

--- edut-youtuber/data/videos.js
+++ edut-youtuber/data/videos.js
@@ -3,14 +3,15 @@
     "id": "video1",
     "title": "¿Qué es la entropía?",
     "channel": "What If Español",
-    "thumbnail": "https://i.ytimg.com/vi/XXXXXXXX/hqdefault.jpg",
+    "thumbnail": "https://www.youtube.com/watch?v=mtA2iIGNCkA.jpg",
     "categories": ["física", "termodinámica"]
   },
   {
     "id": "video2",
     "title": "Introducción a la mecánica cuántica",
     "channel": "QuantumLatam",
-    "thumbnail": "https://i.ytimg.com/vi/YYYYYYYY/hqdefault.jpg",
+    "thumbnail": "https://www.youtube.com/watch?v=mtA2iIGNCkA.jpg",
     "categories": ["física", "cuántica"]
   }
 ]
+
